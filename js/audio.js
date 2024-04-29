00292a2d5259590d9a4719aa0f3be03e846f5fdd
Geoffery Powell <powellgeoffery@gmail.com>
2024-04-29 16:04:40 -0500

    Refactor audio.js to improve audio playback

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,8 @@
 var audio = null;
 
 function playAudio(url) {
-    if (audio && !audio.paused) {
-        return; // Do nothing if audio is already playing
-    }
-    
-    audio = new Audio(url);
+    audio = new Audio();
+    audio.src = url;
+    audio.preload = "auto";
     audio.play();
 }
