72e6632f1cab54e2ecb63d4206020348ae001439
Geoffery Powell <powellgeoffery@gmail.com>
2024-06-15 13:36:56 -0500

    chore: Update font-face declarations in home.html, api_call.js, and styles.css

--- js/api_call.js
+++ js/api_call.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     loadPercentageFunded();
 };
 
@@ -8,26 +8,26 @@ function loadPercentageFunded(retries = 3) {
             "Content-type": "application/json"
         }
     })
-    .then(response => response.json())  // convert to JSON format
-    .then(data => {
-        let percentageFunded = (Math.round(data['data']['percentage_funded'] * 10000) / 100).toFixed(2);
-        document.getElementById('percentage_funded').textContent = percentageFunded + "%";
-        updateDaysRemaining(data);
-        let costPerPerson = (Math.round(data['data']['per_person_required_funding'] * 100) / 100).toFixed(2);
-        document.getElementById('cost_per_person').textContent = "$" + costPerPerson.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    })
-    .catch((error) => {
-        console.error("Error fetching the data:", error);
-        if (retries > 0) {
-            console.log(`Retrying... Attempts left: ${retries - 1}`);
-            setTimeout(() => loadPercentageFunded(retries - 1), 2000);
-        } else {
-            console.log('Failed after 3 attempts');
-            document.getElementById('percentage_funded').textContent = "[FAILED TO LOAD]";
-            document.getElementById('days_remaining').textContent = "[FAILED TO LOAD]";
-            document.getElementById('cost_per_person').textContent = "[FAILED TO LOAD]";
-        }
-    });
+        .then(response => response.json())  // convert to JSON format
+        .then(data => {
+            let percentageFunded = (Math.round(data['data']['percentage_funded'] * 10000) / 100).toFixed(2);
+            document.getElementById('percentage_funded').textContent = percentageFunded + "%";
+            updateDaysRemaining(data);
+            let costPerPerson = (Math.round(data['data']['per_person_required_funding'] * 100) / 100).toFixed(2);
+            document.getElementById('cost_per_person').textContent = "$" + costPerPerson.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        })
+        .catch((error) => {
+            console.error("Error fetching the data:", error);
+            if (retries > 0) {
+                console.log(`Retrying... Attempts left: ${retries - 1}`);
+                setTimeout(() => loadPercentageFunded(retries - 1), 2000);
+            } else {
+                console.log('Failed after 3 attempts');
+                document.getElementById('percentage_funded').textContent = "[FAILED TO LOAD]";
+                document.getElementById('days_remaining').textContent = "[FAILED TO LOAD]";
+                document.getElementById('cost_per_person').textContent = "[FAILED TO LOAD]";
+            }
+        });
 }
 
 function updateDaysRemaining(data) {
